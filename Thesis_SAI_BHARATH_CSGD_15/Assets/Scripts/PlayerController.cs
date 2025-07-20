1d94010e67b88e978646ee76984f2f2903d47207
Saibharath548 <kattavsbharath2004@gmail.com>
2025-07-20 20:21:41 +0530

    Setting up Scriptable Objects and Animator Over Ride Controllers

--- Thesis_SAI_BHARATH_CSGD_15/Assets/Scripts/PlayerController.cs
+++ Thesis_SAI_BHARATH_CSGD_15/Assets/Scripts/PlayerController.cs
@@ -7,36 +7,55 @@ namespace Thesis
     public class PlayerController : MonoBehaviour
     {
         [Header("Private Variables")]
+        [SerializeField] private Camera main_Camera;
         [SerializeField] private Rigidbody player_RigidBody;
         [SerializeField] private Animator player_Animator;
-        [SerializeField] private bool player_Jumping;
+        [SerializeField] private Quaternion player_CurrentRotation;
+        [SerializeField] private Vector3 inputx;
+        [SerializeField] private Vector3 inputz;
         [SerializeField] private float velocity;
-        [SerializeField] private float inputx;
-        [SerializeField] private float inputz;
+        [SerializeField] private float xAxis;
+        [SerializeField] private float yAxis;
+        [SerializeField] private bool player_Jumping;
 
-        [Header("Publicc Variables")]
+
+        [Header("Public  Variables")]
+        public float mouse_Sensitivity;
         public float player_MoveSpeed;
         public float player_JumpForce;
 
         private void Awake()
         {
+            main_Camera = Camera.main;
             player_RigidBody = GetComponent<Rigidbody>();
             player_Animator = GetComponent<Animator>();
         }
 
         private void Start()
         {
+            player_CurrentRotation = gameObject.transform.rotation;
             player_Jumping = false;
         }
-
         private void FixedUpdate()
         {
-            inputx = Input.GetAxis("Horizontal");
-            inputz = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(inputx, 0, inputz) * player_MoveSpeed;
+            inputx = Input.GetAxis("Horizontal") * transform.right;
+            inputz = Input.GetAxis("Vertical") * transform.forward;
+            xAxis = Input.GetAxisRaw("Mouse X") * mouse_Sensitivity;
+            yAxis -= Input.GetAxisRaw("Mouse Y") * mouse_Sensitivity;
+
+            player_CurrentRotation.y += xAxis;
+            yAxis = Mathf.Clamp(yAxis, -20, 20);
+
+            Vector3 movement = (inputx+inputz) * player_MoveSpeed;
             Vector3 FinalVelocity = new Vector3(movement.x, player_RigidBody.velocity.y, movement.z);
+            transform.rotation = Quaternion.Euler(0, player_CurrentRotation.y, 0);
+            main_Camera.transform.localEulerAngles = new Vector3(yAxis, 0, 0);
             player_RigidBody.velocity = FinalVelocity;
+
+
             velocity = movement.magnitude;
+
+
             if (Input.GetKeyDown(KeyCode.Space) && isGrounded() && !player_Jumping)
             {
                 player_RigidBody.AddForce(Vector3.up * player_JumpForce, ForceMode.Impulse);
@@ -48,6 +67,8 @@ namespace Thesis
             }
             player_Animator.SetFloat("xVelocity", velocity);
         }
+
+
 
         private bool isGrounded()
         {
